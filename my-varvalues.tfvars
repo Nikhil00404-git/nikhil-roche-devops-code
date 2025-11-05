0d94fa0e340bc8ea2838e04c11444a7c18d08b42
Nikhil00404-git <nikhilgawande0404@gmail.com>
2025-11-05 07:27:40 +0000

    implemented the variables

---
+++ my-varvalues.tfvars
@@ -0,0 +1,4 @@
+vm-name = "Nikhil-vm-1"
+vm-size = "t2,nano"
+ec2-key-name = "roche-key"
+Nikhil-ami-id="ami-0a25a306450a2cba3"
---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "Nikhil-ami-id" {
+  type = string
+  description = "this is my sydney aws region ami id"
+  #default = "ami-0a25a306450a2cba3"
+}
+
+variable "vm-name" {
+    type = string
+    description = "my aws vm name"
+  
+}
+
+variable "vm-size" {
+    type = string
+    description = "vm size"
+  
+}
+
+variable "ec2-key-name" {
+    type = string
+    description = "ec2-key-name"
+  
+}
